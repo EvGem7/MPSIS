a0b6139994a2c9ac831ee26be8313cedc3b8276c
Eugene <33933105+EvGem7@users.noreply.github.com>
2020-09-23 15:26:56 +0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -23,11 +23,46 @@ int main(void) {
     }
 
     if (s2Prev == 1 && s2Val == 0) {
-      setD1(!getD1());
+    	if (!s1()) {
+    		setD1(!getD1());
+    	}
     }
 
     s2Prev = s2Val;
   }
+}
 
-  return 0;
+/////////////////////////////////////////////
+
+#include "utils.h"
+#include <msp430.h>
+
+#pragma vector = PORT2_VECTOR
+__interrupt void isr(void) {
+	if (!read(P2IFG, BIT2)) {
+		return;
+	}
+
+	delay(TRANSITION_DELAY);
+	if (!s2() && !s1()) {
+		setD1(!getD1());
+	}
+
+	turn(P2IFG, BIT2, 0);
+}
+
+int main(void) {
+  WDTCTL = WDTPW | WDTHOLD;
+
+  configure(P1, BIT7, INPUT);
+  configure(P2, BIT2, INPUT);
+  configure(P1, BIT0, OUTPUT);
+
+  __bis_SR_register(GIE); // Enable global interrupts
+
+  turn(P2IE, BIT2, 1); 	// enable interrupt s2
+  turn(P2IES, BIT2, 0); // back front
+  P1IFG = 0x00;			// Clear all interrupt flags
+
+  __no_operation();
 }
